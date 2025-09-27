ccb490b7e61c1b7935b8a6ac0fe098b4300ff57f
mrRundgren <anders.rundgren@gmail.com>
2025-09-27 13:25:57 +0200

    Code style fixes

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -1,15 +1,12 @@
 using BlazorStatic;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+
 using WebApp.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.UseStaticWebAssets();
 
-builder.Services.AddBlazorStaticService(opt =>
+builder.Services.AddBlazorStaticService(_ =>
 {
     //opt. //check to change the defaults
 }
@@ -22,7 +19,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error", true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -34,11 +31,11 @@ app.UseAntiforgery();
 
 app.MapRazorComponents<App>();
 
-app.UseBlazorStaticGenerator(shutdownApp: !app.Environment.IsDevelopment());
+app.UseBlazorStaticGenerator(!app.Environment.IsDevelopment());
 
 app.Run();
 
-public static class WebsiteKeys
+internal static class WebsiteKeys
 {
     public const string GitHubRepo = "https://github.com/BlazorStatic/WebApp";
     public const string X = "https://x.com/";
